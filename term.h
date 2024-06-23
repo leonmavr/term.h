5b46e6b3ddf87c2b9b0eeed251576e571dbffdd0
Leo Mav <13514096+leonmavr@users.noreply.github.com>
2024-06-23 20:21:39 +0200

    add header

---
+++ term.h
@@ -0,0 +1,36 @@
+#include <cstdio>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+#ifdef __cplusplus
+extern "C" {
+//----------------------------------------------------------------------------------
+// Linux POSIX terminal manipulation macros
+//----------------------------------------------------------------------------------
+#define TERM_ROWS 0
+#define TERM_COLS 0
+#define TERM_CLEAR() printf("\033[H\033[J")
+#define TERM_GOTO_TOPLEFT() printf("\033[0;0H")
+#define TERM_HIDE_CURSOR() printf("\e[?25l")
+#define TERM_SHOW_CURSOR() printf("\e[?25h")
+#define TERM_INIT() do {                                                \
+	TERM_CLEAR();                                                       \
+	TERM_HIDE_CURSOR();                                                 \
+	TERM_GOTO_TOPLEFT();                                                \
+    struct winsize w;                                                   \
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &w) == -1) {                   \
+        perror("ERROR: ioctl can't capture terminal's rows/columns.");  \
+        return 1;                                                       \
+    }                                                                   \
+	g_termRows = w.ws_row;                                              \
+	g_termCols = w.ws_col;                                              \
+} while(0)
+#define TERM_PRINT_AT(x, y, c) printf("\033[%d;%dH%c", (x), (y), (c));
+
+/* terminal's rows */
+int g_termRows = 0;
+/* terminal's columns */
+int g_termCols = 0;
+
+} // extern "C"
+#endif // __cplusplus
