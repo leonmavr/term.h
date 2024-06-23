c50b67d3b4f71a72feac6348f40710edc4dd967f
Leo Mav <13514096+leonmavr@users.noreply.github.com>
2024-06-23 20:26:09 +0200

    fix indentation

--- term.h
+++ term.h
@@ -14,16 +14,16 @@ extern "C" {
 #define TERM_HIDE_CURSOR() printf("\e[?25l")
 #define TERM_SHOW_CURSOR() printf("\e[?25h")
 #define TERM_INIT() do {                                                \
-	TERM_CLEAR();                                                       \
-	TERM_HIDE_CURSOR();                                                 \
-	TERM_GOTO_TOPLEFT();                                                \
+    TERM_CLEAR();                                                       \
+    TERM_HIDE_CURSOR();                                                 \
+    TERM_GOTO_TOPLEFT();                                                \
     struct winsize w;                                                   \
     if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &w) == -1) {                   \
         perror("ERROR: ioctl can't capture terminal's rows/columns.");  \
         return 1;                                                       \
     }                                                                   \
-	g_termRows = w.ws_row;                                              \
-	g_termCols = w.ws_col;                                              \
+    g_termRows = w.ws_row;                                              \
+    g_termCols = w.ws_col;                                              \
 } while(0)
 #define TERM_PRINT_AT(x, y, c) printf("\033[%d;%dH%c", (x), (y), (c));
 
